bc100f6794e188e9158a260e239408bdcb027069
Andrey Dyldin <and@cesbo.com>
2019-06-18 13:04:45 +0500

    new docs, tests for readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,87 +1,114 @@
-//! ## Into
+//! # error-rules
 //!
-//! error-rules is a derive macro to implement error handler based on the enum.
+//! [![Latest Version](https://img.shields.io/crates/v/error-rules.svg)](https://crates.io/crates/error-rules)
+//! [![docs](https://docs.rs/error-rules/badge.svg)](https://docs.rs/error-rules)
 //!
-//! Implements next interfaces:
+//! ## Intro
 //!
-//! - `std::fmt::Display` for all enum variants
-//! - `std::error::Error` and `std::convert::From` for source errors
+//! error-rules is a derive macro to implement error handler.
+//! Error handler based on the enum.
+//! Macro automatically implements conversion of any error type into the inner enum field.
 //!
-//! ## Declaring error
+//! ## Error conversion
+//!
+//! `#[error_from]` attribute implements an automatically conversion from any error type.
+//! Converted type should implements `std::error::Error` itnerface.
 //!
 //! ```rust
-//! use std::io;
 //! use error_rules::*;
 //!
 //! #[derive(Debug, Error)]
-//! enum HumanError {
-//!     #[error_from("Human IO => {}", 0)]
-//!     Io(io::Error),
-//!     #[error_kind("Human => not found")]
-//!     NotFound,
+//! enum AppError {
+//!     #[error_from("App IO: {}", 0)]
+//!     Io(std::io::Error),
 //! }
 //!
-//! #[derive(Default)]
-//! struct Human;
+//! type Result<T> = std::result::Result<T, AppError>;
 //!
-//! impl Human {
-//!     pub fn invoke_failure(&self) -> Result<(), HumanError> {
-//!         Err(io::Error::from(io::ErrorKind::PermissionDenied))?;
-//!         unreachable!()
-//!     }
+//! fn example() -> Result<()> {
+//!     let _file = std::fs::File::open("not-found.txt")?;
+//!     unreachable!()
+//! }
+//!
+//! let error = example().unwrap_err();
+//! assert_eq!(error.to_string().as_str(),
+//!     "App IO: No such file or directory (os error 2)");
+//! ```
+//!
+//! ## Custom error kind
+//!
+//! `#[error_kind]` attribute describes custom error kind.
+//! Could be defined without fields or with fields tuple.
+//!
+//! ```rust
+//! use error_rules::*;
+//!
+//! #[derive(Debug, Error)]
+//! enum AppError {
+//!     #[error_kind("App: error without arguments")]
+//!     E1,
+//!     #[error_kind("App: code:{} message:{}", 0, 1)]
+//!     E2(usize, String),
+//! }
+//!
+//! type Result<T> = std::result::Result<T, AppError>;
+//!
+//! fn example_1() -> Result<()> {
+//!     Err(AppError::E1)
+//! }
+//!
+//! fn example_2() -> Result<()> {
+//!     Err(AppError::E2(404, "Not Found".to_owned()))
+//! }
+//!
+//! let error = example_1().unwrap_err();
+//! assert_eq!(error.to_string().as_str(),
+//!     "App: error without arguments");
+//!
+//! let error = example_2().unwrap_err();
+//! assert_eq!(error.to_string().as_str(),
+//!     "App: code:404 message:Not Found");
+//! ```
+//!
+//! ## Display attributes
+//!
+//! `#[error_from]` and `#[error_kind]` contain list of attributes to display error.
+//! First attribute should be literal string. Other attributes is a number of the
+//! unnamed field in the tuple. Started from 0.
+//!
+//! ## Error chain
+//!
+//! By implementing error for nested modules the primary error handler returns full chain of the error.
+//!
+//! ```rust
+//! use error_rules::*;
+//!
+//! #[derive(Debug, Error)]
+//! enum ModError {
+//!     #[error_from("Mod IO: {}", 0)]
+//!     Io(std::io::Error),
+//! }
+//!
+//! fn mod_example() -> Result<(), ModError> {
+//!     let _file = std::fs::File::open("not-found.txt")?;
+//!     unreachable!()
 //! }
 //!
 //! #[derive(Debug, Error)]
-//! enum BikeError {
-//!     #[error_from("Bike IO => {}", 0)]
-//!     Io(io::Error),
-//!     #[error_from("Bike => {}", 0)]
-//!     Human(HumanError),
-//!     #[error_kind("Bike => speed limit")]
-//!     SpeedLimit,
+//! enum AppError {
+//!     #[error_from("App: {}", 0)]
+//!     Mod(ModError),
 //! }
 //!
-//! #[derive(Default)]
-//! struct Bike(Human);
-//!
-//! impl Bike {
-//!     pub fn chained_failure(&self) -> Result<(), BikeError> {
-//!         self.0.invoke_failure()?;
-//!         unreachable!()
-//!     }
-//!
-//!     pub fn invoke_failure(&self) -> Result<(), BikeError> {
-//!         Err(BikeError::SpeedLimit)?;
-//!         unreachable!()
-//!     }
+//! fn app_example() -> Result<(), AppError> {
+//!     mod_example()?;
+//!     unreachable!()
 //! }
 //!
-//! let b = Bike::default();
-//!
-//! let error = b.chained_failure().unwrap_err();
+//! let error = app_example().unwrap_err();
 //! assert_eq!(error.to_string().as_str(),
-//!     "Bike => Human IO => permission denied");
-//!
-//! let error = b.invoke_failure().unwrap_err();
-//! assert_eq!(error.to_string().as_str(),
-//!     "Bike => speed limit");
+//!     "App: Mod IO: No such file or directory (os error 2)");
 //! ```
-//!
-//! ## error_from
-//!
-//! error_from attribute implements a converter from any error type into `Error`.
-//! Converted type should implements `std::error::Error` itnerface.
-//!
-//! ## error_kind
-//!
-//! error_kind attribute describes additional variant for `Error`.
-//! Could be defined without fields or with fields tuple
-//!
-//! ## display attributes
-//!
-//! `error_from` and `error_kind` contain list of attributes to display error.
-//! First attribute should be literal string. Other attributes is a number of the
-//! unnamed field in the tuple. Started from 0.
 
 extern crate proc_macro;
 
